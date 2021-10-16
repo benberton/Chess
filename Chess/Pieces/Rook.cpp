b75cdb22315ae0043591e3244610bac835dd3b0b
nathanmautz <nmautz@zagmail.gonzaga.edu>
2021-10-16 16:35:24 -0700

    rook movement implemented

--- Chess/Pieces/Rook.cpp
+++ Chess/Pieces/Rook.cpp
@@ -5,7 +5,64 @@
 #include "Rook.h"
 
 bool Rook::isValidMove(Tile *destination) {
-    //TODO
+    if(destination->getX() == tile->getX() or destination->getY() ==tile->getY())
+    {
+        Piece* otherPiece = destination->getPiece();
+        if(otherPiece != nullptr)
+        {
+            if(otherPiece->getTeam() == getTeam())
+            {
+                return false;
+            }
+        } //Checks if destination has piece of the same team
+
+        if(destination->getX() != tile->getX())
+        {
+            int smaller,larger,dir;
+            if(destination->getX() > tile->getX())
+            {
+                dir = 1;
+                smaller = tile->getX();
+                larger = destination->getX();
+            }else
+            {
+                dir = -1;
+                smaller = destination->getX();
+                larger = tile->getX();
+            }
+            Board* board = Board::getInstance();
+            for(int i = smaller+1; i < larger; i+=dir)
+            {
+                if(board->getTile(i,tile->getY())->hasPiece())
+                    return false;
+            }
+
+        }else
+        {
+            int smaller,larger,dir;
+            if(destination->getY() > tile->getY())
+            {
+                dir = 1;
+                smaller = tile->getY();
+                larger = destination->getY();
+            }else
+            {
+                dir = -1;
+                smaller = destination->getY();
+                larger = tile->getY();
+            }
+            Board* board = Board::getInstance();
+            for(int i = smaller+1; i < larger; i+=dir)
+            {
+                if(board->getTile(tile->getX(),i)->hasPiece())
+                    return false;
+            }
+
+        }
+
+
+
+    }
     return false;
 }
 
