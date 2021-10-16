39ab3520320604f7772518b040caca7005dc64f7
Nathan Mautz <nathanmautz@Nathans-MacBook-Air.local>
2021-10-16 11:39:56 -0700

    made piece class

---
+++ Chess/Piece.h
@@ -0,0 +1,35 @@
+//
+// Created by Nathan Mautz on 10/16/21.
+//
+
+#ifndef CHESS_PIECE_H
+#define CHESS_PIECE_H
+
+enum Team{white, black};
+
+class Piece {
+
+public:
+
+    Piece(const Tile &tile, Team team) : tile(tile), team(team) {
+
+    }
+
+    void takePiece();
+    void death();
+
+
+
+    virtual isValidMove(Tile destinaton) = 0;
+    virtual move(Tile destination) = 0;
+
+
+
+private:
+    const char symbol;
+    Tile tile;
+    Team team;
+};
+
+
+#endif //CHESS_PIECE_H
