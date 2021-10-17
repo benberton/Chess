7212ddb7bd80ff0a39ecfc3ac1fe3d4ce98c877a
BenBerton <bberton@outlook.com>
2021-10-16 21:10:14 -0700

    setup pieces

---
+++ cmake-build-debug/CMakeFiles/Chess.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chess.dir/Chess/main.cpp.obj"
+  "Chess.exe"
+  "Chess.exe.manifest"
+  "Chess.pdb"
+  "libChess.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
